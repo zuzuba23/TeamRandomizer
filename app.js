634745999423fd4034fada73bc5b8bada2296233
zuzuba23 <tavi2303@yahoo.com>
2017-03-05 22:08:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(http);
 var util = require('util');
 var express = require('express');
 
